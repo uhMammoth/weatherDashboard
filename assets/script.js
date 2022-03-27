19d99526d8f55c95750d6c05baaeb65a6eac3060
Kyle <kyl3p3tty@gmail.com>
2022-03-26 18:52:55 -0700

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@ var queryWeather = function(){
     var cityName = $('#city-selected').val();
     cityName.replace(" ", '+');
     console.log(cityName);
-    // var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+ cityName +"&appid=8abfb1fec87fdcfb0046a4eafb7171fb";
+    // var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+ cityName +"&appid=";
     // console.log(apiUrl);
 }
 
